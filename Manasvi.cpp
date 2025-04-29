9c89d0ced1e19912066bda39eaf26f0758a6349e
Kavyan Nayak <kavyannayak104@gmail.com>
2025-04-29 17:17:54 +0530

    Ari op

---
+++ Manasvi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // cout << "Hello World" ;
+
+
+    //usse input
+    int a,b, c;
+
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    c =  a + b;
+
+    cout << "Sum is : "<< c;
+
+
+}
